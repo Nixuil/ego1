a4f0e74c7463d70977ce2ed1a100d119d90a0b96
bzy <1540168298@qq.com>
2020-09-18 16:13:36 +0800

    初始环境

---
+++ ego-item/ego-item-interface/src/main/java/com/ego/item/EgoItemInterface.java
@@ -0,0 +1,12 @@
+package com.ego.item;
+
+/**
+ *
+ **/
+//@SpringBootApplication
+//@EnableDiscoveryClient
+//public class EgoItemService {
+//    public static void main(String[] args) {
+//        SpringApplication.run(EgoItemService.class, args);
+//    }
+//}
